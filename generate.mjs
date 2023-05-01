3528d92a7e3053775356fdb0bce851c6990fd5c9
tunnckoCore <5038030+tunnckoCore@users.noreply.github.com>
2023-05-01 04:10:09 +0300

    chore: reset
    Signed-off-by: tunnckoCore <5038030+tunnckoCore@users.noreply.github.com>

--- generate.mjs
+++ generate.mjs
@@ -3,24 +3,24 @@ import fs from "node:fs";
 // const collection = JSON.parse(fs.readFileSync("./collection.json", "utf-8"));
 const _files = fs.readdirSync("./data");
 
-// function generateTokenFiles(files) {
-//   const tokens = files.map((file) => {
-//     const id = file.replace(".html", "");
-//     const json = JSON.parse(fs.readFileSync(`./___json/${id}.json`, "utf-8"));
+function generateTokenFiles(files) {
+  const tokens = files.map((file) => {
+    const id = file.replace(".html", "");
+    const json = JSON.parse(fs.readFileSync(`./tokens/${id}.json`, "utf-8"));
 
-//     const token = {
-//       id,
-//       traits: json.token.traits,
-//       inscriber: null,
-//     };
+    const token = {
+      id,
+      traits: json.traits,
+      inscriber: null,
+    };
 
-//     fs.mkdirSync(`./tokens`, { recursive: true });
-//     fs.writeFileSync(`./tokens/${id}.json`, JSON.stringify(token, null, 2));
+    fs.mkdirSync(`./tokens`, { recursive: true });
+    fs.writeFileSync(`./tokens/${id}.json`, JSON.stringify(token, null, 2));
 
-//     return token;
-//   });
+    return token;
+  });
 
-//   return tokens;
-// }
+  return tokens;
+}
 
-// generateTokenFiles(_files);
+generateTokenFiles(_files);
